836c27f367419cf8155182c5753013f1bb1200ab
CDinatale <cora.dinatale@gmail.com>
2020-11-29 16:29:48 -0500

    updated recipe and ingredient database

--- screens/Intro.js
+++ screens/Intro.js
@@ -62,45 +62,45 @@ const IntroScreen = ({navigation}) => {
             tx.executeSql(
               'INSERT INTO table_recipe (recipe_title, recipe_description, recipe_ingredients, recipe_level, recipe_cookTime, recipe_imageUrl) VALUES (?,?,?,?,?,?)',
               [
-                'Roast Chicken',
-                '\n1. Put Pepper and Salt on the Chicken \n2. Put Chicken in over at 450 F \n3. Cook the chicken for 40 min',
-                'Chicken, Salt, Pepper',
-                'Easy',
-                '20 min',
+                'Perfect Roast Chicken',
+                '\n1. Preheat oven to 350 degrees F (175 degrees C). \n2. Place chicken in a roasting pan, and season generously inside and out with salt and pepper. Sprinkle inside and out with onion powder. Place 3 tablespoons margarine in the chicken cavity. Arrange dollops of the remaining margarine around the chickens exterior. Cut the celery into 3 or 4 pieces, and place in the chicken cavity. \n3. Bake uncovered 1 hour and 15 minutes in the preheated oven, to a minimum internal temperature of 180 degrees F (82 degrees C). Remove from heat, and baste with melted margarine and drippings. Cover with aluminum foil, and allow to rest about 30 minutes before serving. ',
+                '1 whole chicken, Salt, Pepper, 1 tablespoon onion powder, 1/2 cup margarine, 1 stalk celery',
+                'Intermediate',
+                '1 hour 40 min',
                 'food1.jpg',
               ],
             );
             tx.executeSql(
               'INSERT INTO table_recipe (recipe_title, recipe_description, recipe_ingredients, recipe_level, recipe_cookTime, recipe_imageUrl) VALUES (?,?,?,?,?,?)',
               [
-                'Roast Chicken',
-                '\n1. Put Pepper and Salt on the Chicken \n2. Put Chicken in over at 450 F \n3. Cook the chicken for 40 min',
-                'Chicken, Salt, Pepper',
+                'Stove top Steak',
+                '\n1. Liberally coat the steaks with the salt. \n2. Heat a 12-inch cast iron skillet over high heat for about 10 minutes; the pan should smoke just a bit when it is properly heated. \n3. Cook the chicken for 40 min \n4. Carefully place the steaks in the hot pan and cook on the first side until enough of a crust has developed that the steaks no longer stick to the pan, about 1 minute. Flip and cook on the other side for 1 minute. Continue cooking and flipping for a total of 4 minutes. \n5. Carefully add the butter, garlic, and herbs to the pan. Flip the steaks once more. Tilt the pan so the butter pools on one side and use a large spoon to baste the butter over the steaks. Flip again and repeat. Begin checking the internal temperature of the steaks at 6 minutes total cook time for your preferred doneness. Medium rare is between 125°F and 130°F.',
+                '2 New York Steaks, Salt, 3 tablespoons butter, 3 cloves garlic, 2 springs fresh rosemary',
                 'Easy',
-                '20 min',
-                'food1.jpg',
+                '30 min',
+                'food9.jpg',
               ],
             );
             tx.executeSql(
               'INSERT INTO table_recipe (recipe_title, recipe_description, recipe_ingredients, recipe_level, recipe_cookTime, recipe_imageUrl) VALUES (?,?,?,?,?,?)',
               [
-                'Roast Chicken',
-                '\n1. Put Pepper and Salt on the Chicken \n2. Put Chicken in over at 450 F \n3. Cook the chicken for 40 min',
-                'Chicken, Salt, Pepper',
+                'Garlic Broccoli',
+                '\n1. In a Dutch oven, bring 1/2 in. of water to a boil. Add broccoli; cover and cook for 3-5 minutes or until crisp-tender; drain. Mix remaining ingredients; toss with broccoli.',
+                '13 cups fresh broccoli florets, 3 tablespoons butter, 5 garlic cloves, salt',
                 'Easy',
-                '20 min',
-                'food1.jpg',
+                '5 min',
+                'food10.jpg',
               ],
             );
             tx.executeSql(
               'INSERT INTO table_recipe (recipe_title, recipe_description, recipe_ingredients, recipe_level, recipe_cookTime, recipe_imageUrl) VALUES (?,?,?,?,?,?)',
               [
-                'Roast Chicken',
-                '\n1. Put Pepper and Salt on the Chicken \n2. Put Chicken in over at 450 F \n3. Cook the chicken for 40 min',
-                'Chicken, Salt, Pepper',
+                'Honey Garlic Salmon',
+                '\n1. Season salmon with salt and pepper. \n2. Add oil to a large skillet over medium high heat. \n3. Once hot add in the salmon skin side down. \n4 Cook salmon for 5-7 minutes on skin side (depending on size). \n5 Very carefully flip salmon. If the skin is sticking to the pan give it more time to cook. It should release easily from the pan. \n6 Add garlic to the pan and cook for 1 minute. \n7 Combine honey and lemon juice in a small bowl. Add the sauce to the pan. \n8. Continue cooking for 1-3 minutes or until the salmon is fully cooked and looks opaque and the sauce is thick. \n9 Take off of the heat. When the salmon is cool enough to handle peel off the skin. \n10. Serve immediately with a sprinkle of chopped parsley for garnish, if desired.  ',
+                '4 salmon filets, 1 tablespoon butter, 3 garlic cloves, 3 tablespoons honey, 2 tablespoons lemon juice',
                 'Easy',
                 '20 min',
-                'food1.jpg',
+                'food11.jpg',
               ],
             );
           }
@@ -133,11 +133,23 @@ const IntroScreen = ({navigation}) => {
             );
             tx.executeSql(
               'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['margarine', 'dairy'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['butter', 'dairy'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
               ['beef', 'meat'],
             );
             tx.executeSql(
               'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
               ['chicken', 'meat'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['salmon', 'meat'],
             );
             tx.executeSql(
               'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
@@ -149,6 +161,10 @@ const IntroScreen = ({navigation}) => {
             );
             tx.executeSql(
               'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['lemon', 'fruit'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
               ['broccoli', 'vegetable'],
             );
             tx.executeSql(
@@ -157,11 +173,39 @@ const IntroScreen = ({navigation}) => {
             );
             tx.executeSql(
               'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['celery', 'vegetable'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['garlic', 'vegetable'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
               ['bread', 'grain'],
             );
             tx.executeSql(
               'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
               ['wheat', 'grain'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['salt', 'spices'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['pepper', 'spices'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['onion powder', 'spices'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['rosemary', 'vegetable'],
+            );
+            tx.executeSql(
+              'INSERT INTO table_ingredients (ingredient_name, category_name) VALUES (?,?)',
+              ['honey', 'sweeteners'],
             );
           }
         },
